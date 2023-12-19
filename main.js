aee78cf78d7db958fc8a2a5615174c295c3aa985
xDarks010 <132127895+xDarks010@users.noreply.github.com>
2023-12-19 17:42:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300, 300)
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300 , 300);
+    video.hide();
+
+}
+
+function draw(){
+}
+
+function Saveimg(){
+}
